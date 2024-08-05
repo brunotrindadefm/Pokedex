af1e25a0e44286a0b3c4ab168d78a868bb1c48c9
btrindadefm <btrindade172@gmail>
2024-08-05 14:25:09 -0300

    feat: quase acabado componente pokemon

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,28 +1,25 @@
-import { useState, useEffect } from "react"
-import { replace, useParams } from "react-router-dom"
-
-import axios from "axios"
-import PokemonCard from "../components/PokemonCard/PokemonCard"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import './Pokemon.scss';
 
 const Pokemon = () => {
-
   const { id } = useParams();
   const [pokemon, setPokemon] = useState(null);
   const [description, setDescription] = useState(null);
-  const [category, setCategory] = useState(null)
+  const [weaknesses, setWeaknesses] = useState([]);
+  const [strengths, setStrengths] = useState([]);
+  const [category, setCategory] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const getPokemon = async () => {
-
     setLoading(true);
     setError(null);
 
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
       setPokemon(response.data);
-
-
     } catch (err) {
       setError(err);
       console.error(err);
@@ -47,42 +44,198 @@ const Pokemon = () => {
     }
   };
 
+  const getPokemonWeaknesses = async (types) => {
+    try {
+      const typeResponses = await Promise.all(
+        types.map(type => axios.get(`https://pokeapi.co/api/v2/type/${type.type.name}`))
+      );
+
+      const typeWeaknesses = typeResponses.map(response => {
+        const damageRelations = response.data.damage_relations;
+        return {
+          doubleDamageFrom: new Set(damageRelations.double_damage_from.map(type => type.name)),
+          halfDamageFrom: new Set(damageRelations.half_damage_from.map(type => type.name)),
+          noDamageFrom: new Set(damageRelations.no_damage_from.map(type => type.name))
+        };
+      });
+
+      let combinedWeaknesses = new Set([...typeWeaknesses[0].doubleDamageFrom]);
+
+      typeWeaknesses.slice(1).forEach(typeWeakness => {
+        combinedWeaknesses = new Set([...combinedWeaknesses, ...typeWeakness.doubleDamageFrom]);
+      });
+
+      const halfAndNoDamage = new Set(
+        typeWeaknesses.flatMap(typeWeakness => [...typeWeakness.halfDamageFrom, ...typeWeakness.noDamageFrom])
+      );
+
+      const filteredWeaknesses = [...combinedWeaknesses].filter(type => !halfAndNoDamage.has(type));
+
+      setWeaknesses(filteredWeaknesses);
+    } catch (error) {
+      console.error('Error fetching Pokémon weaknesses:', error);
+    }
+  };
+
+  const getPokemonStrengths = async (types) => {
+    try {
+      // Obter informações sobre todos os tipos do Pokémon
+      const typeResponses = await Promise.all(
+        types.map(type => axios.get(`https://pokeapi.co/api/v2/type/${type.type.name}`))
+      );
+
+      // Mapeie as forças de todos os tipos
+      const typeStrengths = typeResponses.map(response => {
+        const damageRelations = response.data.damage_relations;
+        return {
+          doubleDamageTo: new Set(damageRelations.double_damage_to.map(type => type.name)),
+          halfDamageTo: new Set(damageRelations.half_damage_to.map(type => type.name)),
+          noDamageTo: new Set(damageRelations.no_damage_to.map(type => type.name))
+        };
+      });
+
+      console.log('Type Strengths:', typeStrengths);
+
+      // Inicializar conjunto de forças com todas as forças do primeiro tipo
+      let combinedStrengths = new Set();
+      typeStrengths.forEach(typeStrength => {
+        typeStrength.doubleDamageTo.forEach(type => combinedStrengths.add(type));
+      });
+
+      console.log('Combined Strengths:', [...combinedStrengths]);
+
+      // Identificar forças mitigadas
+      const halfAndNoDamage = new Set(
+        typeStrengths.flatMap(typeStrength => [...typeStrength.halfDamageTo, ...typeStrength.noDamageTo])
+      );
+
+      console.log('Half and No Damage:', [...halfAndNoDamage]);
+
+      // Filtrar forças removendo as que são mitigadas
+      const filteredStrengths = [...combinedStrengths].filter(type => !halfAndNoDamage.has(type));
+
+      console.log('Filtered Strengths:', filteredStrengths);
+
+      // Atualizar o estado com os tipos de força
+      setStrengths(filteredStrengths);
+
+    } catch (error) {
+      console.error('Error fetching Pokémon strengths:', error);
+    }
+  };
+
   useEffect(() => {
     getPokemon();
-    getPokemonDescription();
-  }, [id])
+  }, [id]);
 
+  useEffect(() => {
+    if (pokemon) {
+      getPokemonDescription();
+      getPokemonWeaknesses(pokemon.types);
+      getPokemonStrengths(pokemon.types);
+    }
+  }, [pokemon]);
 
-  console.log(pokemon)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const capitalizeFirstLetter = (string) => {
+    if (!string) return '';
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   return (
     <div className="app">
       <div className="container">
         {pokemon && (
-          <div>
-            <div>
+          <div className="about-pokemon">
+            <div className="name-id">
               <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
-              <h3 className='id'>{pokemon.id}</h3>
+              <h4 className='id'>N° {String(pokemon.id).padStart(3, '0')}</h4>
             </div>
-            <div>
+            <div className="img-description">
               <div>
                 <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
-              </div>
-              <div>
-                <p>{description}</p>
                 <div>
-                  <p><strong>Height:</strong> {pokemon.height / 10} m</p>
-                  <p><strong>Weight:</strong> {pokemon.weight / 10} kg</p>
-                  <p><strong>Category:</strong> {category}</p>
+                  {pokemon.types.map((type) => (
+                    <span key={type.type.name} className={`type ${type.type.name}`}>
+                      {type.type.name}
+                    </span>
+                  ))}
                 </div>
               </div>
+              <p>{description}</p>
+              <div className="text-about">
+                <div className="about">
+                  <div>
+                    <p><strong>Height</strong></p>
+                    <span>{pokemon.height / 10} m</span>
+                    <p><strong>Weight</strong></p>
+                    <span>{pokemon.weight / 10} kg</span>
+                  </div>
+                  <div>
+                    <p><strong>Category</strong></p>
+                    <span>{category}</span>
+                    <p><strong>Ability</strong></p>
+                    <span>{pokemon.abilities.length > 0 ? capitalizeFirstLetter(pokemon.abilities[0].ability.name) : 'No abilities available'}</span>
+                  </div>
+                </div>
+                <div className="types">
+                  <div>
+                    <h3>Weaknesses</h3>
+                    {weaknesses.length > 0 ? (
+                      weaknesses.map((weakness, index) => (
+                        <span key={index} className={`type ${weakness}`}>
+                          {weakness}
+                        </span>
+                      ))
+                    ) : (
+                      <p>No weaknesses available.</p>
+                    )}
+                  </div>
+                  <div>
+                    <h3>Strengths</h3>
+                    {strengths.length > 0 ? (
+                      strengths.map((strength, index) => (
+                        <span key={index} className={`type ${strength}`}>
+                          {strength}
+                        </span>
+                      ))
+                    ) : (
+                      <p>No strengths available.</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="stats-container">
+              <h4>Stats</h4>
+              <ul className="stats-list">
+                {pokemon.stats.map(stat => (
+                  <li key={stat.stat.name} className="stats-item">
+                    <span className="stat-name">{capitalizeFirstLetter(stat.stat.name)}:</span>
+                    <div className="stat-bar">
+                      <div
+                        className="stat-bar-fill"
+                        style={{ width: `${stat.base_stat / 2}%` }} 
+                      >
+                      </div>
+                    </div>
+                    <span className="stat-value">{stat.base_stat}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h4>Moves:</h4>
+              <p>{pokemon.moves.slice(0, 10).map(move => move.move.name).join(', ')}</p>
             </div>
           </div>
         )}
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Pokemon
+export default Pokemon;
